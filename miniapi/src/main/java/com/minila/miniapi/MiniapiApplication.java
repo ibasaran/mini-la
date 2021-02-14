12e3ed78d1505dba6f749b2330f6760d6dadc06a
İsmail BAŞARAN <basaran.ismaill@gmail.com>
2021-02-14 20:19:04 +0300

    api module

---
+++ miniapi/src/main/java/com/minila/miniapi/MiniapiApplication.java
@@ -0,0 +1,13 @@
+package com.minila.miniapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniapiApplication.class, args);
+	}
+
+}
---
+++ miniapi/src/test/java/com/minila/miniapi/MiniapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minila.miniapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
